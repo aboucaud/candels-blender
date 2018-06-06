b4c187ed70157650d51b389cc1e067910cf93883
Alexandre Boucaud <boucaud.alexandre@gmail.com>
2018-06-07 00:19:40 +0200

    Fix Galaxy property swap

--- blender/catalog.py
+++ blender/catalog.py
@@ -8,13 +8,13 @@ CATALOG_HEADER = (
     "id",
     "distance",
     "g1_id",
-    "g1_rad",
     "g1_mag",
+    "g1_rad",
     "g1_z",
     "g1_type",
     "g2_id",
-    "g2_rad",
     "g2_mag",
+    "g2_rad",
     "g2_z",
     "g2_type")
 
@@ -35,7 +35,7 @@ def gal2cat(gal: Galaxy) -> List[str]:
 
 def blend2cat(blend: Blend, idx: int) -> List[str]:
     """
-    blend_id distance gal1_id gal1_rad gal1_mag gal1_z gal1_type gal2_id gal2_rad gal2_mag gal2_z gal2_type  # noqa
+    blend_id distance gal1_id gal1_mag gal1_rad gal1_z gal1_type gal2_id gal2_mag gal2_rad gal2_z gal2_type  # noqa
     """
     distance = np.hypot(*blend.shift)
     blendinfo = [
